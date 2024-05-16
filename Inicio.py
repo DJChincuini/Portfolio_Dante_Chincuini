5ef3702e9320902a8edaeb2f3fd091d6a44c9142
DJChincuini <daantechincuini42@gmail.com>
2024-05-16 12:26:20 -0300

    Links en Main.py

--- Inicio.py
+++ Inicio.py
@@ -5,3 +5,24 @@ import plotly.express as px
 
 # Configuro para que el layout sea "wide"
 st.set_page_config(layout="wide")
+
+# TAIL ---------------------------------------------------------------------------------------------------------------
+
+# Creo dos columnas
+tail = st.columns(2)
+
+with tail[0]:
+    pass
+
+with tail[1]:
+    # Creo dos sub columnas
+    sub = st.columns(4)
+    
+    with sub[0]:
+        pass
+    with sub[1]:
+        pass
+    with sub[2]:
+        st.link_button("LinkedIn", "https://www.linkedin.com/in/dante-chincuini-2828b6281/")
+    with sub[3]:
+        st.link_button("GitHub", "https://github.com/DJChincuini")
