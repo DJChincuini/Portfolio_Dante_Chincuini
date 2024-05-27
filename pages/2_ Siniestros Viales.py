8e3644b1d4b605999337560d4a0fafd5e5cb743e
DJChincuini <daantechincuini42@gmail.com>
2024-05-27 16:39:15 -0300

    .

--- pages/2_ Siniestros Viales.py
+++ pages/2_ Siniestros Viales.py
@@ -170,8 +170,8 @@ comuna_filtradas = st.multiselect(
     )
 
 df_filtrado = df_final[(df_final['COMUNA'].isin(comuna_filtradas)) & (df_final['GRUPO ETARIO'].isin(edades_filtradas))]
-# ----------------------------------------------------------------------------------------------
 
+# ----------------------------------------------------------------------------------------------
 # Creo dos columnas
 superior = st.columns(2) 
 
