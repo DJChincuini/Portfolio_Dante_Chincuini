10bd5a0c21a09e0e82713f05e764e7ca2041d36b
DJChincuini <daantechincuini42@gmail.com>
2024-06-09 02:40:59 -0300

    .

--- pages/2_ Siniestros Viales.py
+++ pages/2_ Siniestros Viales.py
@@ -179,7 +179,9 @@ st.write(
     f'''
     \n ###
 
-**Primer KPI:** Para que se cumpla el primer KPIs, el segundo semestre debería haber una cantidad de casos menor a 49.5. Hubo, en cambio, una cantidad de 42 casos. Este corresponde al 76.3% de los casos ocurridos en el primer semestre.
+**Primer KPI:**
+
+Para que se cumpla el primer KPIs, el segundo semestre debería haber una cantidad de casos menor a 49.5. Hubo, en cambio, una cantidad de 42 casos. Este corresponde al 76.3% de los casos ocurridos en el primer semestre.
 
 Si bien el objetivo se alcazó, bajando considerablemente los homicidios en siniestros, se debe comprender que  el valor sigue siendo alto, por lo que sería optimo que se realicen más controles e inversión en infraestructura de seguridad para bajar aún más el número de casos.
 
