5ca6ec1b9d3d3bba7943e3878be698347bc63df8
DJChincuini <daantechincuini42@gmail.com>
2024-06-10 18:19:24 -0300

    Final

--- Inicio.py
+++ Inicio.py
@@ -11,19 +11,15 @@ st.set_page_config(layout="wide")
 
 st.write("## Sobre Mí")
 
-body = st.columns(2)
 
-with body[0]:
-    st.write(
+st.write(
         '''
         📌 Me llamo Dante. Soy un estudiante apasionado de programación y la estadística con un enfoque especial en el diseño, desarrollo e implementación de soluciones robustas por medio del Data Engineering 💻 así cómo también de la búsqueda de insights a través de los datos y del pensamiento analítico a la hora de la toma de decisiones 📊.
         
         📈 Me considero experto en la creación de arquitecturas escalables para procesamiento gracias a mí conocimientos de GCP y Python, cómo también del análisis de datos y el diseño de estrategias ayudándome con mi conocimiento de SQL y PowerBI. También poseo conocimientos en estadística y marketing gracias a mí tecnicatura en Diseño y Comunicación Multimedial.
         
         💪 Siempre en busca de desafíos que impulsen la innovación y la eficiencia en el mundo de la tecnología, mí objetivo es poder colaborar con profesionales tan apasionados cómo yo en pos de impulsar la toma de decisiones empresariales basándonos en lo que los datos pueden proveernos.
-        '''
-    )
-st.write('#')
+        ''')
 
 st.write('#')
 
